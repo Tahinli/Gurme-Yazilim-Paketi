e6e84e4d6e0d0fc9c10226fe4d2ecdf085dac397
Skeior <talhakarasu@icloud.com>
2023-12-08 00:33:08 +0300

    feat: project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "../src/Login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
