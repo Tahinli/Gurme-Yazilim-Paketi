54ad1f5ad13a913d90773c6b06baa26be1a86f3a
Skeior <talhakarasu@icloud.com>
2023-12-08 02:30:16 +0300

    feat: login control structure

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -2,13 +2,27 @@ import "./Login.css";
 import TextField from "@mui/material/TextField";
 import logo from "../assets/img/logo.png";
 import Button from "@mui/material/Button";
+import React, { useState } from "react";
 
 function Login() {
+  // Use state to manage the password
+  const [password, setPassword] = useState("");
+  let isAuthenticated = false;
+  function controlPassword() {
+    if (password === "123") {
+      isAuthenticated = true;
+      alert(isAuthenticated);
+    } else {
+      isAuthenticated = false;
+      alert("Giriş Başarısız");
+    }
+  }
+
   return (
     <div className="loginDiv">
       <div className="mainDiv">
         <div className="logodiv">
-          <img className="imglogo" src={logo} />
+          <img className="imglogo" src={logo} alt="Logo" />
         </div>
         <div className="textfield">
           <TextField
@@ -21,15 +35,21 @@ function Login() {
           <TextField
             type="password"
             className="textf"
-            id="outlined-basic"
+            id="sifre"
             label="Şifre"
             variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // Update the password state on input change
             required
           />
         </div>
         <br />
         <div className="buttonDiv">
-          <Button variant="contained" className="submitButton">
+          <Button
+            variant="contained"
+            className="submitButton"
+            onClick={controlPassword}
+          >
             Giriş Yap
           </Button>
         </div>
