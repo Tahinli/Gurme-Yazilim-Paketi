94c53a1faf71066c4d939f2f2612c80356502d80
Buse Duran <buseduran500@gmail.com>
2023-12-18 00:42:45 +0300

    feat: use http in api fetch

--- src/api/handler/handler.js
+++ src/api/handler/handler.js
@@ -2,20 +2,15 @@ import axios from 'axios';
 
 const handleRequest = async (options) => {
     try {
-        const response = await axios(options);
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            // The request was made, but the server responded with a status code
-            // that falls out of the range of 2xx
-            throw `Error: ${error.response.status} - ${error.response.statusText}`;
-        } else if (error.request) {
-            // The request was made, but no response was received
-            throw 'No response received from the server';
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            throw 'Error setting up the request';
+        const response = await fetch(options.url, options);
+
+        if (!response.ok) {
+            throw `Error: ${response.status} - ${response.statusText}`;
         }
+
+        return await response.json();
+    } catch (error) {
+        throw error.message || 'Unable to connect to the server!';
     }
 };
 
