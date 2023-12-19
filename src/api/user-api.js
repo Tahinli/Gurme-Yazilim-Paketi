d8696249fae60905a0d413e41da3c2750449d9d1
Buse Duran <buseduran500@gmail.com>
2023-12-19 11:55:57 +0300

    feat: gunluk-api updated

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -1,92 +1,92 @@
 import apiURL from "./_apiURL.js";
 import handleRequest from "./handler/handler.js";
 const getUsers = async () => {
-  try {
-    const options = {
-      method: "GET",
-      url: `${apiURL}/kullanici/hepsi`,
-      json: true,
-    };
-    const result = await handleRequest(options);
+    try {
+        const options = {
+            method: "GET",
+            url: `${apiURL}/kullanici/hepsi`,
+            json: true,
+        };
+        const result = await handleRequest(options);
 
-    return result;
-  } catch (error) {
-    throw error;
-  }
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
 const getUserById = async (id) => {
-  try {
-    const options = {
-      method: "GET",
-      url: `${apiURL}/kullanici/${id}`,
-      json: true,
-    };
+    try {
+        const options = {
+            method: "GET",
+            url: `${apiURL}/kullanici/${id}`,
+            json: true,
+        };
 
-    const result = await handleRequest(options);
-    return result;
-  } catch (error) {
-    throw error;
-  }
+        const result = await handleRequest(options);
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const addUser = async (userData) => {
-  const isim = userData.isim;
-  const soyisim = userData.soyisim;
-  const id = userData.id;
-  const sifre = userData.sifre;
-  try {
-    const options = {
-      method: "GET",
-      url: `${apiURL}/kullanici/ekle/${isim}/${soyisim}/${id}/${sifre}`,
-      json: true,
-      body: userData,
-    };
-    const result = await handleRequest(options);
-    return result;
-  } catch (error) {
-    throw error;
-  }
+    const isim = userData.isim;
+    const soyisim = userData.soyisim;
+    const id = userData.id;
+    const sifre = userData.sifre;
+    try {
+        const options = {
+            method: "GET",
+            url: `${apiURL}/kullanici/ekle/${isim}/${soyisim}/${id}/${sifre}`,
+            json: true,
+            body: userData,
+        };
+        const result = await handleRequest(options);
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const deleteUser = async (id) => {
-  try {
-    const options = {
-      method: "GET",
-      url: `${apiURL}/kullanici/sil/${id}`,
-      json: true,
-    };
-    const result = await handleRequest(options);
-    return result;
-  } catch (error) {
-    throw error;
-  }
+    try {
+        const options = {
+            method: "GET",
+            url: `${apiURL}/kullanici/sil/${id}`,
+            json: true,
+        };
+        const result = await handleRequest(options);
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const updateUser = async (id, userData) => {
-  const yeni_isim = userData.isim;
-  const yeni_soyisim = userData.soyisim;
-  const yeni_id = userData.id;
-  const yeni_sifre = userData.sifre;
-  try {
-    const options = {
-      method: "GET",
-      url: `${apiURL}/kullanici/duzenle/${id}/${yeni_isim}/${yeni_soyisim}/${yeni_id}/${yeni_sifre}`,
-      json: true,
-      body: userData,
-    };
-    const result = await handleRequest(options);
-    return result;
-  } catch (error) {
-    throw error;
-  }
+    const yeni_isim = userData.isim;
+    const yeni_soyisim = userData.soyisim;
+    const yeni_id = userData.id;
+    const yeni_sifre = userData.sifre;
+    try {
+        const options = {
+            method: "GET",
+            url: `${apiURL}/kullanici/duzenle/${id}/${yeni_isim}/${yeni_soyisim}/${yeni_id}/${yeni_sifre}`,
+            json: true,
+            body: userData,
+        };
+        const result = await handleRequest(options);
+        return result;
+    } catch (error) {
+        throw error;
+    }
 };
 
 const kullaniciApi = {
-  getUserById,
-  getUsers,
-  addUser,
-  deleteUser,
-  updateUser,
+    getUserById,
+    getUsers,
+    addUser,
+    deleteUser,
+    updateUser,
 };
 
 export default kullaniciApi;
