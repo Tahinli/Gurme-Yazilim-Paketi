3a6cff48ecc93aca1d650edfe910c6c783be3841
Skeior <talhakarasu@icloud.com>
2023-12-20 03:17:13 +0300

    feat:Update CSS styles and function calls

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
       }
     );
     navigate("/");
-    await verifyUser();
+    verifyUser();
   };
   const verifyUser = async () => {
     const response = await fetch(URL + "/verify", {
@@ -47,8 +47,8 @@ const Navbar = () => {
       },
       credentials: "include",
     });
-    await setVerify(response.status === 200);
-    await setVerify1(response.status !== 200);
+    setVerify(response.status === 200);
+    setVerify1(response.status !== 200);
   };
   const buttonClick = () => {
     navigate("/login");
