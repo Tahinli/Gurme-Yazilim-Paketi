0956ad69ed4833f03bd5eebb34e0c18721b1d2d1
Skeior <talhakarasu@icloud.com>
2023-12-21 23:48:55 +0300

    refactor: AuthProvider and update Login component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import AuthProvider from "./AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
