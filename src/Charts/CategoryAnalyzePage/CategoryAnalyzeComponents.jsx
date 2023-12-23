11dd49c1bc0926d0505a5b2334e7588abe4b0252
Barış Onur Yıldız <86011311+barisonuryy@users.noreply.github.com>
2023-12-23 16:30:02 +0300

    fix:updated categoryAnalyze script

--- src/Charts/CategoryAnalyzePage/CategoryAnalyzeComponents.jsx
+++ src/Charts/CategoryAnalyzePage/CategoryAnalyzeComponents.jsx
@@ -11,25 +11,29 @@ import Checkbox from '@mui/material/Checkbox';
 import urunApi from "../../api/urun-api.js";
 import gunlukApi from "../../api/gunluk-api.js";
 import kategoriApi from "../../api/kategori-api.js";
-import {ca, da} from "date-fns/locale";
+import {ca, da, de} from "date-fns/locale";
 import Gunluk from "../../api/gunluk-api.js";
+import { useNavigate } from 'react-router'
+import {useLocation} from "react-router-dom";
+
 
 const productList = await urunApi.getUrunler();
 const logList = await gunlukApi.getGunlukler();
 // const urungir = (await urunApi.getUrunler()).map((urun) => urun.isim);
 const catList = (await kategoriApi.getKategoriler()).map((kategori) => kategori.isim);
 
+let arrProduct = [];
+for (let i = 0; i < 30; i++) {
+    arrProduct[i] = [];
+    for (let j = 0; j < catList.length; j++) {
+        arrProduct[i][j] = 0;
+    }
+}
+const zort=arrProduct
+console.log(arrProduct)
 
-
-//console.log(testData)
-
-console.log(catList)
-console.log(catList.length)
-let tempH=0,tempD=0,tempDrink=0
-
-let totalHamL=[],totalDesertL=[],totalDrinkL=[];
 let range
-var parsedData=dataJS;
+
 const date=new Date()
 
 var datePerc=date.getUTCDay()
@@ -57,6 +61,9 @@ var weekData = catList.map((label, index) => ({
     label: label
 }));
 await filterByCatWeek()
+const dynamicSeries = [];
+await monthlyBarChart()
+
 
 function getTodayDate() {
     const today = new Date();
@@ -69,9 +76,8 @@ function convertDate(date){
 }
 
 async function filterByCatDaily() {
-    const todayLog = logList.filter(gunluk => gunluk.tarih === getTodayDate());
 
-    console.log(todayLog.length);
+    const todayLog = logList.filter(gunluk => gunluk.tarih === getTodayDate());
 
     for (let j = 0; j < catList.length; j++) {
         let totalVal = 0;
@@ -81,17 +87,15 @@ async function filterByCatDaily() {
 
             if (catList[j] === productCategory.kategori.isim) {
                 count++
-                console.log(log);
-                console.log(productCategory);
                 totalVal += log.ulasilan / log.hedeflenen;
-                console.log(`totalVal: ` + totalVal);
             }
         }));
 
         DailyData.find(predicate => predicate.label === catList[j]).value = (totalVal/count)*100;
+
+
     }
 
-    console.log(DailyData);
 }
 async function createData(){
     const [data,setData]=useState([])
@@ -106,10 +110,8 @@ async function filterByCatMonth()
     date2.setMonth(date2.getMonth()+1)
     date2.setDate(date2.getDate()-1)
 
-    console.log("Tarih 1:"+date1+"Tarih 2 :"+date2)
-
     const rangeLog=logList.filter(gunluk=>(convertDate(gunluk.tarih)>=date1&&convertDate(gunluk.tarih)<=date2))
-    console.log(convertDate('23.12.2023')<convertDate('25.12.2023'))
+
     for (let j = 0; j < catList.length; j++) {
         let totalVal = 0;
         let count=0
@@ -118,16 +120,46 @@ async function filterByCatMonth()
 
             if (catList[j] === productCategory.kategori.isim) {
                 count++
-                console.log("AylıkLog:"+MonthLog);
-                console.log(productCategory);
                 totalVal += MonthLog.ulasilan / MonthLog.hedeflenen;
-                console.log(`totalVal: ` + totalVal);
             }
         }));
 
         monthData.find(predicate => predicate.label === catList[j]).value = (totalVal/count)*100;
     }
 
+}
+async function monthlyBarChart(){
+
+    let todayValues=getTodayDate().split('.')
+    const date1=new Date(`${todayValues[2]}-${todayValues[1]}-${todayValues[0]}`)
+    const tempDate=new Date()
+    for(let i=0;i<30;i++){
+        date1.setDate(tempDate.getDate()-i)
+        let lastDay=date1.toLocaleString('tr-TR', { year: 'numeric', month: '2-digit', day: '2-digit' })
+        const rangeLog = logList.filter(gunluk =>gunluk.tarih=== lastDay);
+        for (let j = 0; j < catList.length; j++) {
+            let tempVal=0
+            let count=0
+            await Promise.all(rangeLog.map(async (barLog) => {
+                        const productCategory = await urunApi.getUrunByName(barLog.urun_isim);
+
+                        if (catList[j] === productCategory.kategori.isim) {
+                            count++
+                            tempVal += barLog.ulasilan / barLog.hedeflenen;
+                        }
+
+                    }
+                )
+            );
+            tempVal/=count
+            tempVal*=100
+            arrProduct[i][j]=tempVal
+            if(isNaN(arrProduct[i][j]))
+                arrProduct[i][j]=0
+
+        }
+    }
+    return arrProduct
 }
 async function filterByCatWeek()
 {
@@ -137,12 +169,7 @@ async function filterByCatWeek()
     date1.setDate(date1.getDate()-(datePerc-1))
     const date2=new Date(date1)
     date2.setDate(date2.getDate()+6)
-    let d1=date1.toLocaleString('tr-TR', { year: 'numeric', month: '2-digit', day: '2-digit' });
-    let d2 = date2.toLocaleString('tr-TR', { year: 'numeric', month: '2-digit', day: '2-digit' });
-    const firstRange=convertDate(d1)
-    const lastRange=convertDate(d2)
-    console.log("İlk Date Değer:"+date1+"2. Date Değer:"+date2)
-    const rangeLog=logList.filter(gunluk=>(convertDate(gunluk.tarih)>=firstRange&&convertDate(gunluk.tarih)<=lastRange))
+    const rangeLog=logList.filter(gunluk=>(convertDate(gunluk.tarih)>=date1&&convertDate(gunluk.tarih)<=date2))
     for (let j = 0; j < catList.length; j++) {
         let totalVal = 0;
         let count=0
@@ -151,10 +178,7 @@ async function filterByCatWeek()
 
             if (catList[j] === productCategory.kategori.isim) {
                 count++
-                console.log(WeekLog);
-                console.log(productCategory);
                 totalVal += WeekLog.ulasilan / WeekLog.hedeflenen;
-                console.log(`totalVal: ` + totalVal);
             }
         }));
 
@@ -162,8 +186,35 @@ async function filterByCatWeek()
     }
 
 }
+const refreshPage = () => {
+    window.location.reload();
+
+};
+function refresh(){
+    const location = useLocation();
+
+    useEffect(() => {
+        // Sadece belirli bir sayfada çalışmasını istiyorsanız
+        if (location.pathname === '/belirli-sayfa') {
+            // localStorage'da bir anahtar var mı kontrol et
+            const hasVisitedBefore = localStorage.getItem('hasVisitedBefore');
+
+            if (!hasVisitedBefore) {
+                // Eğer daha önce ziyaret edilmemişse sayfa yenileme işlemi gerçekleştir
+                window.location.reload();
+
+                // localStorage'a ziyaret edildi bilgisini kaydet
+                localStorage.setItem('hasVisitedBefore', 'true');
+            }
+        }
+    }, [location.pathname]);
+}
+//refresh()
+
 //GÜNLÜK HESAPLAMA
 export  function  CategoryDailyAnalyzeComp() {
+
+
     const data=DailyData
     return (
         <>
@@ -249,7 +300,7 @@ export function CategoryMonthlyAnalyzeComp() {
 //SON 30 GÜN İÇİN AYARLANABİLİR BAR CHART
 export function BarAnimation() {
 
-    const [seriesNb, setSeriesNb] =useState(3);
+    const [seriesNb, setSeriesNb] =useState(catList.length);
     const [itemNb, setItemNb] = useState(30);
     const [skipAnimation, setSkipAnimation] = useState(false);
     range=itemNb
@@ -268,73 +319,18 @@ export function BarAnimation() {
         setSeriesNb(newValue);
     };
 
-    for(let j=range;j>0;j--){
 
-        const dateH=new Date()
-        dateH.setDate(dateH.getDate()-j)
-        tempH=0
-        const ham = parsedData.filter(function (product) {
+    for (let seriesIndex = 0; seriesIndex < seriesNb; seriesIndex++) {
+        const label = catList[seriesIndex];
+        const data = [];
+        for (let dataIndex = range; dataIndex >0; dataIndex--) {
 
-            const productDate = new Date(product.Year, product.Month - 1, product.Day);
-            return productDate.getUTCMonth() === dateH.getUTCMonth() && productDate.getUTCDate() === dateH.getUTCDate() && productDate.getUTCFullYear() === dateH.getUTCFullYear() && product.Category === "Hamur"
-        });
-        for(let i=0;i<ham.length;i++){
-
-            tempH+=(ham[i].CompletedCount/ham[i].GoalCount);
+            data.push( Number(zort[dataIndex-1][seriesIndex]));
 
         }
-
-        tempH=tempH /ham.length
-        if(ham.length!==0){
-            totalHamL[j]=tempH
-        }
-        else{
-            totalHamL[j]=Number(0)
-        }
-        tempDrink=0
-
-        const icecek = parsedData.filter(function (product) {
-
-
-            const productDate = new Date(product.Year, product.Month - 1, product.Day);
-            return productDate.getUTCMonth() === dateH.getUTCMonth() && productDate.getUTCDate() === dateH.getUTCDate() && productDate.getUTCFullYear() === dateH.getUTCFullYear() && product.Category === "İçecek"
-        });
-
-        for(let i=0;i<icecek.length;i++){
-            tempDrink+=(icecek[i].CompletedCount/icecek[i].GoalCount);
-
-        }
-        tempDrink/=icecek.length
-        if(icecek.length!==0){
-            totalDrinkL[j]=tempDrink
-        }
-        else{
-            totalDrinkL[j]=Number(0)
-        }
-
-
-
-
-
-        tempD=0
-        const tatli = parsedData.filter(function (product) {
-            const productDate = new Date(product.Year, product.Month - 1, product.Day);
-            return productDate.getUTCMonth() === dateH.getUTCMonth() && productDate.getUTCDate() === dateH.getUTCDate() && productDate.getUTCFullYear() === dateH.getUTCFullYear() && product.Category === "Tatlı"
-        });
-        for(let i=0;i<tatli.length;i++){
-            tempD+=(tatli[i].CompletedCount/tatli[i].GoalCount);
-
-        }
-        tempD/=tatli.length
-        if(tatli.length!==0){
-            totalDesertL[j]=tempD
-        }
-        else{
-            totalDesertL[j]=Number(0)
-        }
-
-
+        dynamicSeries.push({ label, data });
     }
+
 
     const highlightScope = {
         highlighted: 'series',
@@ -342,81 +338,7 @@ export function BarAnimation() {
     };
 
 
-    const series = [
-        {
-            label: 'Hamur',
-            data: [
-                totalHamL[range], totalHamL[range-1], totalHamL[range-2], totalHamL[range-3], totalHamL[range-4], totalHamL[range-5], totalHamL[range-6], totalHamL[range-7], totalHamL[range-8], totalHamL[range-9], totalHamL[range-10], totalHamL[range-11], totalHamL[range-12],
-                totalHamL[range-13], totalHamL[range-14], totalHamL[range-15],totalHamL[range-16],totalHamL[range-17],totalHamL[range-18],totalHamL[range-19],totalHamL[range-20],totalHamL[range-21],totalHamL[range-22],totalHamL[range-23],totalHamL[range-24],totalHamL[range-25],totalHamL[range-26],totalHamL[range-27],totalHamL[range-28],totalHamL[range-29],
-            ],
-        },
-        {
-            label: 'Tatlı',
-            data: [
-                totalDesertL[range], totalDesertL[range-1], totalDesertL[range-2], totalDesertL[range-3], totalDesertL[range-4], totalDesertL[range-5], totalDesertL[range-6], totalDesertL[range-7], totalDesertL[range-8], totalDesertL[range-9], totalDesertL[range-10], totalDesertL[range-11], totalDesertL[range-12],
-                totalDesertL[range-13], totalDesertL[range-14], totalDesertL[range-15],totalDesertL[range-16],totalDesertL[range-17],totalDesertL[range-18],totalDesertL[range-19],totalDesertL[range-20],totalDesertL[range-21],totalDesertL[range-22],totalDesertL[range-23],totalDesertL[range-24],totalDesertL[range-25],totalDesertL[range-26],totalDesertL[range-27],totalDesertL[range-28],totalDesertL[range-29],
-            ],
-        },
-        {
-            label: 'İçecek',
-            data: [
-                totalDrinkL[range], totalDrinkL[range-1], totalDrinkL[range-2], totalDrinkL[range-3], totalDrinkL[range-4], totalDrinkL[range-5], totalDrinkL[range-6], totalDrinkL[range-7], totalDrinkL[range-8], totalDrinkL[range-9], totalDrinkL[range-10], totalDrinkL[range-11], totalDrinkL[range-12],
-                totalDrinkL[range-13], totalDrinkL[range-14], totalDrinkL[range-15],totalDrinkL[range-16],totalDrinkL[range-17],totalDrinkL[range-18],totalDrinkL[range-19],totalDrinkL[range-20],totalDrinkL[range-21],totalDrinkL[range-22],totalDrinkL[range-23],totalDrinkL[range-24],totalDrinkL[range-25],totalDrinkL[range-26],totalDrinkL[range-27],totalDrinkL[range-28],totalDrinkL[range-29],
-            ],
-        },
-        {
-            label: 'series 4',
-            data: [
-                2361, 979, 2430, 1768, 1913, 2342, 1868, 1319, 1038, 2139, 1691, 935, 2262,
-                1580, 692, 1559, 1344, 1442, 1593, 1889,
-            ],
-        },
-        {
-            label: 'series 5',
-            data: [
-                968, 1371, 1381, 1060, 1327, 934, 1779, 1361, 878, 1055, 1737, 2380, 875, 2408,
-                1066, 1802, 1442, 1567, 1552, 1742,
-            ],
-        },
-        {
-            label: 'series 6',
-            data: [
-                2316, 1845, 2057, 1479, 1859, 1015, 1569, 1448, 1354, 1007, 799, 1748, 1454,
-                1968, 1129, 1196, 2158, 540, 1482, 880,
-            ],
-        },
-        {
-            label: 'series 7',
-            data: [
-                2140, 2082, 708, 2032, 554, 1365, 2121, 1639, 2430, 2440, 814, 1328, 883, 1811,
-                2322, 1743, 700, 2131, 1473, 957,
-            ],
-        },
-        {
-            label: 'series 8',
-            data: [
-                1074, 744, 2487, 823, 2252, 2317, 2139, 1818, 2256, 1769, 1123, 1461, 672,
-                1335, 960, 1871, 2305, 1231, 2005, 908,
-            ],
-        },
-        {
-            label: 'series 9',
-            data: [
-                1792, 886, 2472, 1546, 2164, 2323, 2435, 1268, 2368, 2158, 2200, 1316, 552,
-                1874, 1771, 1038, 1838, 2029, 1793, 1117,
-            ],
-        },
-        {
-            label: 'series 10',
-            data: [
-                1433, 1161, 1107, 1517, 1410, 1058, 676, 1280, 1936, 1774, 698, 1721, 1421,
-                785, 1752, 800, 990, 1809, 1985, 665,
-            ],
-        },
-    ].map((s) => ({ ...s, highlightScope }));
-
-
-
+    const series = dynamicSeries.map((s) => ({ ...s, highlightScope }));
 
     return (
         <Box sx={{ width: '98%'}}>
@@ -457,7 +379,7 @@ export function BarAnimation() {
                     onChange={handleSeriesNbChange}
                     valueLabelDisplay="auto"
                     min={1}
-                    max={10}
+                    max={catList.length}
                     sx={{width:'30%'}}
                     aria-labelledby="input-series-number"
                     color='error'
@@ -467,3 +389,11 @@ export function BarAnimation() {
         </Box>
     );
 }
+
+
+
+
+
+
+
+
--- src/Charts/CategoryAnalyzePage/dateRangeForCategory.jsx
+++ src/Charts/CategoryAnalyzePage/dateRangeForCategory.jsx
@@ -1,15 +1,58 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import * as React from 'react';
 import {PieChart} from "@mui/x-charts/PieChart";
 import {dataJS} from "../dataJS.js";
 import {Button, responsiveFontSizes} from "@mui/material";
 import { Card } from '@mui/joy';
 import CancelIcon from '@mui/icons-material/Cancel';
-
+import urunApi from "../../api/urun-api.js";
+import kategoriApi from "../../api/kategori-api.js";
+import gunlukApi from "../../api/gunluk-api.js";
+const catList = (await kategoriApi.getKategoriler()).map((kategori) => kategori.isim);
+const logList = await gunlukApi.getGunlukler();
 const parsedData= dataJS
 let valHam,valDesert,valDrink;
+var firstDate,lastDate
+let f1,f2
+function setDate(fD,lD){
+    f1=fD
+    f2=lD
+}
+console.log("HOHOHOHO:"+f1)
+var rangeData = catList.map((label, index) => ({
+    id: index,
+    value: 0,
+    label: label
+}));
+await filterByCatRange(f1,f2)
+function convertDate(date){
+    return new Date(date.split('.').reverse().join('-'))
+}
+console.log("İLK DATE DEĞERİ:"+firstDate)
+async function filterByCatRange(date1,date2)
+{
+    console.log("Değerler1"+date1)
+    console.log("Değerler2"+date2)
+    const rangeLog=logList.filter(gunluk=>(convertDate(gunluk.tarih)>=date1&&convertDate(gunluk.tarih)<=date2))
+    console.log(rangeLog)
+    for (let j = 0; j < catList.length; j++) {
+        let totalVal = 0;
+        let count=0
+        await Promise.all(rangeLog.map(async (rangeLog) => {
+            const productCategory = await urunApi.getUrunByName(rangeLog.urun_isim);
+
+            if (catList[j] === productCategory.kategori.isim) {
+                count++
+                totalVal += rangeLog.ulasilan / rangeLog.hedeflenen;
+            }
+        }));
+
+        rangeData.find(predicate => predicate.label === catList[j]).value = (totalVal/count)*100;
+    }
+
+}
 
 export  function DateRangeP() {
     let totalHamR=0,totalDesertR=0,totalDrinkR=0;
@@ -26,44 +69,23 @@ export  function DateRangeP() {
         isRefreshed(false)
     }, 5000);
 
-    const firstDate = new Date(startDate.getUTCFullYear(),startDate.getUTCMonth(),startDate.getUTCDate())
-    const lastDate = new Date(endDate.getUTCFullYear(),endDate.getUTCMonth(),endDate.getUTCDate())
+    firstDate = new Date(startDate.getUTCFullYear(),startDate.getUTCMonth(),startDate.getUTCDate())
+    lastDate = new Date(endDate.getUTCFullYear(),endDate.getUTCMonth(),endDate.getUTCDate())
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // İlk date ve last date'i buraya taşıyabilirsiniz
+                const firstDate = new Date(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate());
+                const lastDate = new Date(endDate.getUTCFullYear(), endDate.getUTCMonth(), endDate.getUTCDate());
 
-    const hamR = parsedData.filter(function (product) {
+                await filterByCatRange(firstDate, lastDate);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData()
+    }, [firstDate,lastDate]);
 
-        let productDate = new Date(product.Year,product.Month-1,product.Day);
-
-        return ((productDate>=firstDate&&productDate<=lastDate)&&product.Category === "Hamur")
-
-    });
-    for(let i=0;i<hamR.length;i++){
-
-
-        totalHamR+=(hamR[i].CompletedCount/hamR[i].GoalCount);
-    }
-    totalHamR/=hamR.length
-
-
-    const icecekR = parsedData.filter(function (product) {
-        let productDate = new Date(product.Year,product.Month-1,product.Day);
-
-        return ((productDate>=firstDate&&productDate<=lastDate) && product.Category==="İçecek")
-    });
-
-    for(let i=0;i<icecekR.length;i++){
-        totalDrinkR+=(icecekR[i].CompletedCount/icecekR[i].GoalCount);
-    }
-    totalDrinkR/=icecekR.length
-
-    const tatliR = parsedData.filter(function (product) {
-        let productDate = new Date(product.Year,product.Month-1,product.Day);
-
-        return ((productDate>=firstDate&&productDate<=lastDate) && product.Category==="Tatlı")
-    });
-    for(let i=0;i<tatliR.length;i++){
-        totalDesertR+=(tatliR[i].CompletedCount/tatliR[i].GoalCount);
-    }
-    totalDesertR/=tatliR.length
     function setValAnalyze(bool){
         setAnalyze(bool)
         isRefreshed(bool)
@@ -75,28 +97,23 @@ export  function DateRangeP() {
         valDrink=totalDrinkR
     }
 
-    const data = [
-        { id: 0, value: valHam, label: 'Hamur' },
-        { id: 1, value: valDesert, label: 'Tatlı' },
-        { id: 2, value: valDrink, label: 'İçecek' },
-    ];
+    const data=rangeData
     const close_datepicker = () => {
         setAnalyze(false);
-      };
+    };
 
     return (
         <div className={"ChartsDate"}>
-            <div >
-                <Card className="Filter_part"
-                      style={{width: '30%'}}
+            <div>
+                <Card style={{width: '30%', height: '150px'}}
                       sx={{alignItems:'center'}}
-                      color="warning"
+                      color="neutral"
                       invertedColors={false}
                       orientation="vertical"
                       size="lg"
                       variant="soft"
                 >
-                     Filtrele:
+                    Filtrele:
                     <div className={"dateArea"} style={{display:'flex'}}>
                         <DatePicker
                             selected={startDate}
@@ -114,17 +131,18 @@ export  function DateRangeP() {
                             minDate={startDate}
                         />
                     </div>
-                        <Button onClick={() => setValAnalyze(true)} variant="contained" color="warning">ANALİZ</Button>
+                    <Button onClick={() => setValAnalyze(true)} variant="contained" color="warning">ANALİZ</Button>
+
                 </Card>
 
             </div>
             <div>
-                {analyze && <Card className='selected_analyze' style={{width: '65%', height: '400px', marginTop:15}}
+                {analyze && <Card className={"Charts"} style={{width: '65%', height: '400px', marginTop:15}}
                                   color="neutral"
                                   invertedColors={false}
                                   orientation="vertical"
                                   size="lg"
-                                  variant="soft" 
+                                  variant="soft"
                 >
                     <CancelIcon onClick={close_datepicker} variant="contained" color="warning"/>
 
